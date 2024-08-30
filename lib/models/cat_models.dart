0854d8f85af9cfd25fd808ecc07460e248d91d67
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-08-30 13:40:39 +0200

    implement the first 'categories' section

---
+++ lib/models/cat_models.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class Category_Model {
+  String name;
+  String iconPath;
+  Color boxColor;
+
+  Category_Model({
+    required this.name,
+    required this.iconPath,
+    required this.boxColor,
+  });
+
+  static List<Category_Model> getCategories() {
+    List<Category_Model> categories = [];
+
+    categories.add(
+      Category_Model(
+        name: 'Salad',
+        iconPath: 'assets/icons/plate.svg',
+        boxColor: Color(0xFFD7FADA),
+      ),
+    );
+
+    categories.add(
+      Category_Model(
+        name: 'Cake',
+        iconPath: 'assets/icons/pancakes.svg',
+        boxColor: Color(0xFFE4D7FF),
+      ),
+    );
+
+    categories.add(
+      Category_Model(
+        name: 'Pie',
+        iconPath: 'assets/icons/pie.svg',
+        boxColor: Color(0xFFE9F7F1),
+      ),
+    );
+
+    categories.add(
+      Category_Model(
+        name: 'Smoothies',
+        iconPath: 'assets/icons/orange-snacks.svg',
+        boxColor: Color(0xFFE9F7F1),
+      ),
+    );
+
+    return categories;
+  }
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,84 +1,150 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter1/models/cat_models.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: appBar(),
-    backgroundColor: Colors.white,
-    body: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        searchBar(),
-        SizedBox(height: 40,),
-        Column(
+class _HomePageState extends State<HomePage> {
+  List<Category_Model> categories = [];
+
+  void _getCategories() {
+    categories = Category_Model.getCategories();
+  }
+
+  @override
+  void initState() {
+    _getCategories();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _getCategories();
+    return Scaffold(
+      appBar: appBar(),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          searchBar(),
+          SizedBox(height: 40),
+          _categoriesSection(),
+        ],
+      ),
+    );
+  }
+
+  Column _categoriesSection() {
+    return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
-              padding: const EdgeInsets.only(left:20),
-              child: Text(
+              padding: const EdgeInsets.only(left: 20),
+              child: const Text(
                 'Categories',
-                style: const TextStyle(
-                  color:Colors.black,
+                style: TextStyle(
+                  color: Colors.black,
                   fontSize: 18,
                   fontWeight: FontWeight.w600,
                 ),
               ),
             ),
-          ],
-        ),
-      ],
-    ),
-  );
-}
-
-Container searchBar() {
-  return Container(
-        margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
-        decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.11),
-              spreadRadius: 0.5,
-              blurRadius: 35,
-              offset: const Offset(0, 3),
+            SizedBox(height: 15),
+            Container(
+              height: 150,
+              child: ListView.separated(
+                itemCount: categories.length,
+                scrollDirection: Axis.horizontal,
+                separatorBuilder: (context, index) => const SizedBox(width: 25,),
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: 100,
+                    decoration: BoxDecoration(
+                      color: categories[index].boxColor.withOpacity(0.3),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            shape: BoxShape.circle
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: SvgPicture.asset(categories[index].iconPath),
+                          ),
+                        ),
+                        Text(
+                          categories[index].name,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
             ),
           ],
-        ),
-        child: Container(
-          child: TextField(
-            decoration: InputDecoration(
-              filled: true,
-              fillColor: Colors.white,
-              contentPadding: const EdgeInsets.all(15),
-              hintText: 'Search for recipes',
-              hintStyle: const TextStyle(
-                color: Color(0xffB1B1B1),
-                fontSize: 14,
-              ),
-              prefixIcon: Padding(
-                padding: const EdgeInsets.all(12),
-                child: SvgPicture.asset('assets/icons/Search.svg'),
-              ),
-              suffixIcon: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SvgPicture.asset('assets/icons/Filter.svg'),
-              ),
-              border: const OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                borderSide: BorderSide(
-                  width: 0,
-                  style: BorderStyle.none,
-                ),
+        );
+  }
+
+  Container searchBar() {
+    return Container(
+      margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.11),
+            spreadRadius: 0.5,
+            blurRadius: 35,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Container(
+        child: TextField(
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.white,
+            contentPadding: const EdgeInsets.all(15),
+            hintText: 'Search for recipes',
+            hintStyle: const TextStyle(
+              color: Color(0xffB1B1B1),
+              fontSize: 14,
+            ),
+            prefixIcon: Padding(
+              padding: const EdgeInsets.all(12),
+              child: SvgPicture.asset('assets/icons/Search.svg'),
+            ),
+            suffixIcon: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SvgPicture.asset('assets/icons/Filter.svg'),
+            ),
+            border: const OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              borderSide: BorderSide(
+                width: 0,
+                style: BorderStyle.none,
               ),
             ),
           ),
         ),
-      );
-}
+      ),
+    );
+  }
 
   AppBar appBar() {
     return AppBar(
@@ -87,7 +153,7 @@ Container searchBar() {
         style: TextStyle(
             color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
       ),
-      backgroundColor:Colors.white,
+      backgroundColor: Colors.white,
       elevation: 0.0,
       centerTitle: true,
       leading: GestureDetector(
