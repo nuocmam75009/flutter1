28044d31ff79bb2cb2823f7afb6160bf778695a3
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-08-30 11:51:49 +0200

    search text in search bar, cleaned code, and starting 'category' container

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,48 +9,75 @@ class HomePage extends StatelessWidget {
 Widget build(BuildContext context) {
   return Scaffold(
     appBar: appBar(),
+    backgroundColor: Colors.white,
     body: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Container(
-          margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.11),
-                spreadRadius: 0.5,
-                blurRadius: 35,
-                offset: const Offset(0, 3),
+        searchBar(),
+        SizedBox(height: 40,),
+        Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left:20),
+              child: Text(
+                'Categories',
+                style: const TextStyle(
+                  color:Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
-            ],
-          ),
-          child: Container(
-            child: TextField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                contentPadding: EdgeInsets.all(15),
-                prefixIcon: Padding(
-                  padding: const EdgeInsets.all(12),
-                  child: SvgPicture.asset('assets/icons/Search.svg'),
-                ),
-                suffixIcon: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SvgPicture.asset('assets/icons/Filter.svg'),
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: const BorderRadius.all(Radius.circular(10)),
-                  borderSide: const BorderSide(
-                    width: 0,
-                    style: BorderStyle.none,
-                  ),
+            ),
+          ],
+        ),
+      ],
+    ),
+  );
+}
+
+Container searchBar() {
+  return Container(
+        margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.11),
+              spreadRadius: 0.5,
+              blurRadius: 35,
+              offset: const Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Container(
+          child: TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              contentPadding: const EdgeInsets.all(15),
+              hintText: 'Search for recipes',
+              hintStyle: const TextStyle(
+                color: Color(0xffB1B1B1),
+                fontSize: 14,
+              ),
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(12),
+                child: SvgPicture.asset('assets/icons/Search.svg'),
+              ),
+              suffixIcon: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SvgPicture.asset('assets/icons/Filter.svg'),
+              ),
+              border: const OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                borderSide: BorderSide(
+                  width: 0,
+                  style: BorderStyle.none,
                 ),
               ),
             ),
           ),
         ),
-      ],
-    ),
-  );
+      );
 }
 
   AppBar appBar() {
